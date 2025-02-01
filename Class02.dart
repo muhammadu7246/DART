93599c99d7a1b908405aeae4e6ee9af377a11fe5
ameerhamza1123 <hamzamughal63420@gmail.com>
2025-02-01 16:58:24 +0500

    class02

---
+++ Class02.dart
@@ -0,0 +1,18 @@
+void main() {
+  print('Hello, World!');
+// print is a builtin function to print anything on screen
+//data types
+  int num = 60; //integer
+  int num_2 = 50; //integer
+  double double_num = 10.5; //Double/Float
+  bool bool_val = true; //boolean - only has two values: true or false
+  var var_name = 'Learning Dart'; //dynamic data type - Variable
+
+// operators
+// arithmetic operators
+  print(num + num_2); // addition
+  print(num - num_2); // subtraction
+  print(num * num_2); // multiplication
+  print(num / num_2); // division
+  print(num % num_2); // modules
+}

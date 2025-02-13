bd45ef18acd0af749ad9bdb4d6fc5e7b4ae7a370
ameerhamza1123 <hamzamughal63420@gmail.com>
2025-02-13 16:18:38 +0500

    function type

---
+++ Functions_Type.dart
@@ -0,0 +1,18 @@
+void main() {
+// Named parameters
+
+  void bold({int? bold, bool? hidden}) {
+    print(bold);
+  }
+
+  bold(bold: 10);
+
+  // optional parameter
+
+  void function(String name, [String? message]) {
+    print('$message, $name');
+  }
+
+  function('hamza');
+  function('Hi,', 'Good morning');
+}
